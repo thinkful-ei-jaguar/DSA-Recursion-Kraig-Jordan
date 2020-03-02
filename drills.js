f6f809ec7df579cf4e7ca9aab2e25d642a65f088
Jordan Castillo <jordanxcallaway@gmail.com>
2020-03-02 09:13:52 -0800

    drill #1

---
+++ drills.js
@@ -0,0 +1,15 @@
+// #1 : Counting Sheep 
+function countingSheep(numOfSheep) {
+  if(numOfSheep === 0){
+    return 'All sheep jumped over the fence';
+  }
+
+  const str = `${numOfSheep}: Another sheep jumps over the fence \n` ;
+  return str + countingSheep(numOfSheep - 1);
+}
+
+countingSheep(3);
+// => 3: Another sheep jumps over the fence
+// => 2: Another sheep jumps over the fence
+// => 1: Another sheep jumps over the fence
+// => All sheep jumped over the fence
