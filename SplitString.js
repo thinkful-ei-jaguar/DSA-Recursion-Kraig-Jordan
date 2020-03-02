7da76e2fb38f35de99dff5028e275d15d335b963
Jordan Castillo <jordanxcallaway@gmail.com>
2020-03-02 14:21:40 -0800

    split string complete

--- SplitString.js
+++ SplitString.js
@@ -1,26 +1,20 @@
+/* eslint-disable eqeqeq */
 function splitString(str, separator, newArr=[]) {
-  console.log(str, 'string');
   if (str.length == 0) {
     return [...newArr];
   }
 	
   let indexSep = str.indexOf(separator);
-  console.log(indexSep, 'indexSep');
 
   if (indexSep < 0) {
     indexSep = str.length; 
-    console.log(indexSep, 'indexSep in if');
   }
 	
   const newStr = str.substring(0, indexSep);
-  console.log(newStr, 'newStr');
-	
 	
   newArr.push(newStr);
-  console.log(newArr, 'newArr');
 
   str = str.slice(indexSep + 1);
-  console.log(str, 'remainingStr');
 
   return splitString(str, separator, [...newArr]);
 }
